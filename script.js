cf12e5b859cbfc981036ded4f0d7e647c3d83d61
Mayuri Gore <67511992+Mayurigore2502@users.noreply.github.com>
2023-07-30 21:54:54 +0530

    Add files via upload
    Hello..Here I want to declare that, I uploaded half material of website...I am currently working on this website .So I will upload  whole  code list..

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript code
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navToggle = document.querySelector('.nav-toggle');
+navToggle.addEventListener('click', function() {
+  nav.classList.toggle('open');
+ggle('show');
+});
